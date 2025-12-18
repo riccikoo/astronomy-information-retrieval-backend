44939707eb3644aa8def7a6e63545824e75359c4
riccikoo <reivalaura@gmail.com>
2025-12-18 10:26:09 +0700

    preprocessing pdf

---
+++ utils/preprocessing.py
@@ -0,0 +1,11 @@
+import re
+from nltk.corpus import stopwords
+
+stop_words = set(stopwords.words('english'))
+
+def preprocess(text):
+    text = text.lower()
+    text = re.sub(r'[^a-z\s]', '', text)
+    tokens = text.split()
+    tokens = [t for t in tokens if t not in stop_words]
+    return " ".join(tokens)
