eb7481f095142fc250f40c84bdba71de7327dbe8
riccikoo <reivalaura@gmail.com>
2025-12-26 18:28:26 +0700

    preprocessing update with spacy

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,11 +1,23 @@
 import re
+import spacy
 from nltk.corpus import stopwords
 
+nlp = spacy.load("en_core_web_sm", disable=['parser', 'ner'])
 stop_words = set(stopwords.words('english'))
 
 def preprocess(text):
+    # 1. Lowercase
     text = text.lower()
-    text = re.sub(r'[^a-z\s]', '', text)
-    tokens = text.split()
-    tokens = [t for t in tokens if t not in stop_words]
+    
+    # 2. Tangani tanda hubung agar kata seperti "black-hole" jadi "blackhole" atau "black hole"
+    text = re.sub(r'(\w+)-(\w+)', r'\1 \2', text)
+    
+    # 3. Hapus angka dan simbol tapi simpan istilah teknis jika perlu
+    # Disini kita hanya simpan alfabet
+    text = re.sub(r'[^a-z\s]', ' ', text)
+    
+    # 4. Lemmatization dengan spaCy
+    doc = nlp(text)
+    tokens = [token.lemma_ for token in doc if not token.is_stop and len(token.lemma_) > 2]
+    
     return " ".join(tokens)
